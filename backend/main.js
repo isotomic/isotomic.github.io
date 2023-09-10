1bdbb4adf8a40cca8ccbc10650ef6387e25b6670
Eclipse-5214 <83417472+Eclipse-5214@users.noreply.github.com>
2023-09-10 17:31:38 -0400

    Update main.js

--- backend/main.js
+++ backend/main.js
@@ -199,12 +199,13 @@ if (window.location.href === "https://isotomic.github.io/") {
 }
 
 function readTextFile(file) {
+  var allText = null;
   var rawFile = new XMLHttpRequest();
   rawFile.open("GET", file, false);
   rawFile.onreadystatechange = function () {
     if (rawFile.readyState === 4) {
       if (rawFile.status === 200 || rawFile.status == 0) {
-        var allText = rawFile.responseText;
+        allText = rawFile.responseText;
       }
     }
   };
