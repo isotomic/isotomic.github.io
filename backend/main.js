a7d2fd00576ae92de1426ce98830f7dcf64947ce
Eclipse-5214 <83417472+Eclipse-5214@users.noreply.github.com>
2023-09-11 10:33:16 -0400

    Update main.js

--- backend/main.js
+++ backend/main.js
@@ -196,3 +196,31 @@ window.addEventListener(
 if (window.location.href === backendurl) {
   document.getElementById("main").appendChild(dl);
 }
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$wrapper = $('#wrapper'),
+		$header = $('#header'),
+		$footer = $('#footer'),
+		$main = $('#main'),
+		$main_articles = $main.children('article');
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+	// Nav.
+		var $nav = $header.children('nav'),
+			$nav_li = $nav.find('li');
+
+		// Add "middle" alignment classes if we're dealing with an even number of items.
+			if ($nav_li.length % 2 == 0) {
+
+				$nav.addClass('use-middle');
+				$nav_li.eq( ($nav_li.length / 2) ).addClass('is-middle');
+
+			}
+})(jQuery);
