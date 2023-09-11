a8e6277d42ac50e3d2657b924b5459769e760b29
Eclipse-5214 <83417472+Eclipse-5214@users.noreply.github.com>
2023-09-10 20:49:07 -0400

    Create download.js

---
+++ backend/download.js
@@ -0,0 +1,32 @@
+function makeHttpObject() {
+  try {
+    return new XMLHttpRequest();
+  } catch (error) {}
+  try {
+    return new ActiveXObject("Msxml2.XMLHTTP");
+  } catch (error) {}
+  try {
+    return new ActiveXObject("Microsoft.XMLHTTP");
+  } catch (error) {}
+
+  throw new Error("Could not create HTTP request object.");
+}
+
+function saveStaticDataToFile() {
+  var request = makeHttpObject();
+  request.open("GET", "https://isotomic.github.io/", true);
+  request.send(null);
+  request.onreadystatechange = function () {
+    if (request.readyState == 4)
+      var blob = new Blob([request.responseText], {
+        type: "text/plain;charset=utf-8",
+      });
+    var newincog = document.createElement("a");
+    newincog.href = URL.createObjectURL(blob);
+    newincog.download = "isotomic.html";
+    newincog.click();
+    URL.revokeObjectURL(newincog.href);
+  };
+}
+
+saveStaticDataToFile();
