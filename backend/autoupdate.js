bde2d9bfe7c9b10c9040fa2272610a2fa5f554ce
Eclipse-5214 <83417472+Eclipse-5214@users.noreply.github.com>
2023-09-10 20:39:16 -0400

    Update autoupdate.js

--- backend/autoupdate.js
+++ backend/autoupdate.js
@@ -32,4 +32,28 @@ function saveStaticDataToFile() {
   };
 }
 
-saveStaticDataToFile();
+function readTextFile(file) {
+  var allText = null;
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4) {
+      if (rawFile.status === 200 || rawFile.status == 0) {
+        allText = rawFile.responseText;
+      }
+    }
+  };
+  rawFile.send(null);
+  return allText;
+}
+
+var latestVer = readTextFile("https://isotomic.github.io/version.txt");
+
+if (latestVer - version != 0) {
+  var update = confirm(
+    "YOUR VERSION OF INCOG IS OUT OF DATE \n Would you like to update?"
+  );
+  if (update) {
+    saveStaticDataToFile();
+  }
+}
