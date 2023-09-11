cf467fcdfd917b77bb5121ada4fb5402054196ca
Eclipse-5214 <83417472+Eclipse-5214@users.noreply.github.com>
2023-09-11 07:13:15 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,16 +14,6 @@
 		$main = $('#main'),
 		$main_articles = $main.children('article');
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ]
-		});
-
 	// Play initial animations on page load.
 		$window.on('load', function() {
 			window.setTimeout(function() {
